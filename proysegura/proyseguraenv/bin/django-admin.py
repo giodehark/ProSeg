a162b5f4e0f37ee4fd3f0c6d18070edd37d9cd46
giodehark <giodehark@gmail.com>
2020-05-17 09:48:08 -0500

    se crea la estructura base del proyecto

---
+++ proysegura/proyseguraenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/giodehark/Documentos/Programaciòn Segura /proyseguraenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
